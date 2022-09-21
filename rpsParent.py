4f698371b5856a31e9e4128056e8300e1b1d75a1
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-21 15:34:56 -0700

    Moved methods not being used by class children into gameInstance class

--- rpsParent.py
+++ rpsParent.py
@@ -1,38 +1,16 @@
 from gestures import Gestures
-from checkInputFunctions import (checkUserInputYesNo, checkUserInput)
+from checkInputFunctions import *
 
 class RockPaperScissors:
     def __init__(self) :
         self.gestures = Gestures()
-        self.welcomeMessage()
+        
     
 
-    
-    def thisRound():
-        pass
-    
-    
+        
+
     def checkRoundWinner(inputOne, inputTwo):
         if inputOne == inputTwo:
             print ("Tie! Play again!")
-        else:
-            pass
-    
-    def welcomeMessage(self):
-        yesOrNo = input("Hello! Welcome to 'Rock Paper Scissors Lizard Spock'! Would you like to view the rules? ")
-        yesOrNo = checkUserInputYesNo(yesOrNo)
-        if yesOrNo == "yes":
-            print ("""
-                    Rock crushes Scissors
-                    Scissors cuts Paper
-                    Paper covers Rock
-                    Rock crushes Lizard
-                    Lizard poisons Spock
-                    Spock smashes Scissors
-                    Scissors decapitates Lizard
-                    Lizard eats Paper
-                    Paper disproves Spock
-                    Spock vaporizes Rock
-                    """)
-        else:
+        elif inputOne == "rock" and inputTwo == "scissors":
             pass
