3eb3c3b39d7c7a1ac2572185ada86a77082dd854
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-21 16:36:35 -0700

    Added name to welcomeMessage method

--- rpsNPC.py
+++ rpsNPC.py
@@ -1,11 +1,15 @@
 from rpsParent import RockPaperScissors
 import random as r
 
-class RockPaperScissorsNPC(RockPaperScissors):
-    def __init__(self):
+class RockPaperScissorsNPC(RockPaperScissors,):
+    
+    
+    def __init__(self, name):
         super().__init__()
+        self.npc = "NPC"
+        self.name = name
         self.welcomeMessage()
         
     def welcomeMessage(self):
-        print("You have chosen the 1 player game!")
+        print(f"Welcome {self.name}! You have chosen the 1 player game!")        
         
--- rpsTwoPlayer.py
+++ rpsTwoPlayer.py
@@ -1,10 +1,14 @@
 from rpsParent import RockPaperScissors
 
 class RockPaperScissorsTwoPlayer(RockPaperScissors):
-    def __init__(self) -> None:
+    
+    
+    def __init__(self, nameOne, nameTwo):
         super().__init__()
+        self.player_one_name = nameOne
+        self.player_two_name = nameTwo
         self.welcomeMessage()
         
     def welcomeMessage(self):
-        print("You have chosen the 2 player game!")
+        print(f"Welcome {self.player_one_name} and {self.player_two_name}! You have chosen the 2 player game!")
         
