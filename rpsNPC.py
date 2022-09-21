01d6503f185a5c08e80c147713ece76ef0e5f7ab
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-21 15:33:48 -0700

    Added welcomeMessage methods to print which class was selected

--- rpsNPC.py
+++ rpsNPC.py
@@ -1,5 +1,11 @@
 from rpsParent import RockPaperScissors
+import random as r
 
 class RockPaperScissorsNPC(RockPaperScissors):
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
+        self.welcomeMessage()
+        
+    def welcomeMessage(self):
+        print("You have chosen the 1 player game!")
+        
--- rpsTwoPlayer.py
+++ rpsTwoPlayer.py
@@ -3,3 +3,8 @@ from rpsParent import RockPaperScissors
 class RockPaperScissorsTwoPlayer(RockPaperScissors):
     def __init__(self) -> None:
         super().__init__()
+        self.welcomeMessage()
+        
+    def welcomeMessage(self):
+        print("You have chosen the 2 player game!")
+        
