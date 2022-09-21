f2f09693fe4d47ce0566d5fe5e36e95c1a6c413a
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-09-21 15:32:21 -0700

    Changed chooseGesture method to show which player chose which gesture

--- gestures.py
+++ gestures.py
@@ -1,3 +1,19 @@
-class gestures:
-    def __init__(self) -> None:
-        pass
+from checkInputFunctions import *
+
+class Gestures:
+    
+    def __init__(self):
+        self.available_gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.current_gesture = ""
+        
+            
+    def chooseGesture(self, whichPlayer, gestureChoice):
+        self.current_gesture = checkUserInput(gestureChoice)
+        print (f"{whichPlayer} has chosen {self.current_gesture}.")
+        return self.current_gesture
+    
+    def listGestures(self):
+        number = 1
+        for gestures in self.available_gestures:
+            print(f"{number}: {gestures}")
+            number += 1
